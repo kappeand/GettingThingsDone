13c502df84c0365be24d447685ed59ca99022c5a
Andrin Kappeler <kappeand@students.zhaw.ch>
2023-07-04 09:32:10 +0200

    completed crud

--- backend/src/main/java/ch/zhaw/students/gtd/boundary/TaskEndpoint.java
+++ backend/src/main/java/ch/zhaw/students/gtd/boundary/TaskEndpoint.java
@@ -15,13 +15,19 @@ public class TaskEndpoint {
     @Autowired
     private TaskController taskController;
 
+    @RequestMapping(path = "/api/task", method = RequestMethod.PUT)
+    @PreAuthorize("isAuthenticated() AND hasRole('USER')")
+    public void createTask(@RequestBody Task task) {
+        taskController.create(task);
+    }
+
     @RequestMapping(path = "/api/task/{username}", method = RequestMethod.GET)
     @PreAuthorize("isAuthenticated() AND hasRole('USER')")
     public List<Task> getTasksByOwner(@PathVariable String username) {
         return taskController.readByOwner(username);
     }
 
-    @RequestMapping(path = "/api/task", method = RequestMethod.PUT)
+    @RequestMapping(path = "/api/task", method = RequestMethod.POST)
     @PreAuthorize("isAuthenticated() AND hasRole('USER')")
     public void updateTask(@RequestBody Task task) {
         taskController.update(task);
