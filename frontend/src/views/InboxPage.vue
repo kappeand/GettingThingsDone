6d1d99dc37e3ea30bca28ae3b61c6ba3f32c8a7c
Laurent Mouchart <mouchlau@students.zhaw.ch>
2023-07-04 14:45:12 +0200

    Refactored Modal

--- frontend/src/views/InboxPage.vue
+++ frontend/src/views/InboxPage.vue
@@ -6,7 +6,7 @@
           <ion-label>Inbox</ion-label>
         </ion-list-header>
         <div :key="task.id" v-for="task in tasks">
-          <ion-item v-if="!task.done && inboxId == task.projectId">
+          <ion-item id="open-modal" v-on:click="setTask(task)" v-if="!task.done && inboxId == task.projectId">
             <ion-grid>
               <ion-row>
                 <ion-col size="1">
@@ -20,55 +20,55 @@
                                 style="--border-color: green" mode="ios" color="green"
                                 @click="finishTask(task)"></ion-checkbox>
                 </ion-col>
-                <ion-col id="open-modal">
+                <ion-col>
                   {{ task.name }}
                 </ion-col>
               </ion-row>
             </ion-grid>
           </ion-item>
+          <TaskModal :modal-task="task"></TaskModal>
         </div>
       </ion-list>
       <ion-fab slot="fixed" horizontal="end" vertical="bottom">
-        <ion-fab-button id="open-modal" expand="block">
+        <ion-fab-button id="open-modal" v-on:click="setTask(newTask)" expand="block">
           <ion-icon :icon="add"></ion-icon>
         </ion-fab-button>
       </ion-fab>
-      <TaskModal :modal-task="newTask"></TaskModal>
+      <TaskModal :modal-task="modalTask"></TaskModal>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import {
-  IonButton,
   IonCheckbox,
   IonCol,
   IonContent,
-  IonDatetime,
-  IonDatetimeButton,
   IonFab,
   IonFabButton,
   IonGrid,
   IonIcon,
-  IonInput,
   IonItem,
   IonList,
-  IonModal,
   IonPage,
   IonRow,
-  IonSelect,
-  IonSelectOption,
-  IonTextarea,
 } from "@ionic/vue";
 import {add} from 'ionicons/icons';
 import {useTasks} from "../composables/useTasks";
 import {useProjects} from "../composables/useProjects";
-import {Priority} from "../model/task";
+import {Priority, Task} from "../model/task";
 import {onMounted} from "vue";
 import TaskModal from '../components/TaskModal.vue'
 
 const {newTask, tasks, addTask, updateExistingTask, finishTask} = useTasks();
 const {inboxId, projects, getInboxProjectId} = useProjects();
+
+let modalTask = newTask;
+
+function setTask(task: Task) {
+  modalTask.value = task;
+
+}
 
 onMounted(async () => {
   newTask.value.projectId = await getInboxProjectId();
