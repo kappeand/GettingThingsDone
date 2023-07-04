6c1cefd9583224c4a7df4824f7b594fece56e1be
Andrin Kappeler <kappeand@students.zhaw.ch>
2023-07-04 09:59:19 +0200

    renamed get to read

--- backend/src/main/java/ch/zhaw/students/gtd/boundary/TaskEndpoint.java
+++ backend/src/main/java/ch/zhaw/students/gtd/boundary/TaskEndpoint.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.security.Principal;
 import java.util.List;
 
@@ -25,7 +24,7 @@ public class TaskEndpoint {
 
     @RequestMapping(path = "/api/task", method = RequestMethod.GET)
     @PreAuthorize("isAuthenticated() AND hasRole('USER')")
-    public List<Task> getTasksByOwner(Principal principal) {
+    public List<Task> readTasksByOwner(Principal principal) {
         return taskController.readByOwner(principal.getName());
     }
 
