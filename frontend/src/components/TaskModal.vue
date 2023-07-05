057cad1037c29d40cf31f6c47b840493f896e1d4
Laurent Mouchart <mouchlau@students.zhaw.ch>
2023-07-05 11:42:52 +0200

    Implemented Radiobutton in modal view for existing tasks

--- frontend/src/components/TaskModal.vue
+++ frontend/src/components/TaskModal.vue
@@ -1,37 +1,67 @@
 <template>
   <ion-content class="ion-padding">
-    <ion-item>
-      <ion-input type="text" placeholder="Title" v-model="task.name"></ion-input>
-    </ion-item>
-    <ion-item>
-      <ion-textarea type="text" placeholder="Description" v-model="task.description"></ion-textarea>
-    </ion-item>
-    <ion-item>
-      <ion-select v-model="task.projectId" aria-label="project" class="always-flip"
-                  toggleIcon="caret-down-sharp" interface="popover" label="Project">
-        <ion-select-option :value="project.id" :key="project.id" v-for="project in projects">
-          {{ project.name }}
-        </ion-select-option>
-      </ion-select>
-    </ion-item>
-    <ion-item>
-      <ion-select v-model="task.priority" aria-label="priority" class="always-flip"
-                  toggleIcon="caret-down-sharp" interface="popover" label="Priority">
-        <ion-select-option value="HIGH">High</ion-select-option>
-        <ion-select-option value="MEDIUM">Medium</ion-select-option>
-        <ion-select-option value="LOW">Low</ion-select-option>
-      </ion-select>
-    </ion-item>
+    <ion-grid>
+      <ion-row>
+        <ion-col size="1" v-if="!isNewTask" style="padding-top: 4%">
+          <ion-checkbox v-if="task.priority == Priority.HIGH"
+                        style="--checkmark-color: background ;--border-color: red;--border-color-checked: red;--checkbox-background-checked: red"
+                        mode="ios" color="red"
+                        @click="finishTask(task);modalController.dismiss();"></ion-checkbox>
+          <ion-checkbox v-if="task.priority == Priority.MEDIUM"
+                        style="--checkmark-color: background ;--border-color: yellow;--border-color-checked: yellow;--checkbox-background-checked: yellow"
+                        mode="ios" color="yellow"
+                        @click="finishTask(task);modalController.dismiss();"></ion-checkbox>
+          <ion-checkbox v-if="task.priority == Priority.LOW"
+                        style="--border-color: green;--border-color-checked: green;--checkbox-background-checked: green"
+                        mode="ios" color="green"
+                        @click="finishTask(task);modalController.dismiss();"></ion-checkbox>
+        </ion-col>
+        <ion-col>
+          <ion-row>
+            <ion-input v-model="task.name" placeholder="Title" type="text"></ion-input>
+          </ion-row>
+          <ion-row>
+            <ion-datetime-button v-if="task.dueDate != undefined" datetime="datetime"></ion-datetime-button>
+            <ion-datetime-button v-if="task.dueDate == undefined" datetime="datetime"
+                                 :color="'success'"></ion-datetime-button>
+          </ion-row>
+          <ion-modal :keep-contents-mounted="true">
+            <ion-datetime v-model="task.dueDate" id="datetime"></ion-datetime>
+          </ion-modal>
+          <ion-row>
+            <ion-select v-model="task.projectId" aria-label="project" class="always-flip"
+                        toggleIcon="caret-down-sharp" interface="popover" label="Project">
+              <ion-select-option :value="project.id" :key="project.id" v-for="project in projects">{{
+                  project.name
+                }}
+              </ion-select-option>
+            </ion-select>
+          </ion-row>
+          <ion-row>
+            <ion-select v-model="task.priority" aria-label="priority" class="always-flip"
+                        toggleIcon="caret-down-sharp" interface="popover" label="Priority">
+              <ion-select-option value="HIGH">High</ion-select-option>
+              <ion-select-option value="MEDIUM">Medium</ion-select-option>
+              <ion-select-option value="LOW">Low</ion-select-option>
+            </ion-select>
+          </ion-row>
+        </ion-col>
+      </ion-row>
+    </ion-grid>
     <ion-button v-if="isNewTask" @click="handleButton(task)" expand="full">Create Task</ion-button>
     <ion-button v-if="!isNewTask" @click="handleButton(task)" expand="full">Update Task</ion-button>
   </ion-content>
+
 </template>
 <script setup lang="ts">
 import {
-  IonButton,
+  IonButton, IonCheckbox, IonCol,
   IonContent,
+  IonDatetime,
+  IonDatetimeButton, IonGrid,
   IonInput,
   IonItem,
+  IonModal, IonRow,
   IonSelect,
   IonSelectOption,
   IonTextarea,
@@ -43,7 +73,7 @@ import {onMounted, ref} from "vue";
 import {Priority, Task} from "@/model/task";
 
 const {projects, getInboxId} = useProjects();
-const {addOrUpdateTask} = useTasks();
+const {addOrUpdateTask, finishTask} = useTasks();
 const props = defineProps(['modalTask']);
 
 async function handleButton(modalTask: Task) {
@@ -62,4 +92,5 @@ onMounted(async () => {
     task.value.priority = Priority.LOW
   }
 });
+
 </script>
