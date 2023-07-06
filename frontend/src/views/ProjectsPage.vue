98b38a2f4b3cb4445dfc6caa0d78770d44b2803b
Andrin Kappeler <kappeand@students.zhaw.ch>
2023-07-06 10:17:47 +0200

    load projects when entering view

--- frontend/src/views/ProjectsPage.vue
+++ frontend/src/views/ProjectsPage.vue
@@ -1,12 +1,25 @@
 <template>
   <ion-page>
     <ion-content :fullscreen="true">
-      <ProjectList :is-archive="false"/>
+      <ProjectList :is-archive="false" v-if="projectsLoaded"/>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import {IonContent, IonPage} from '@ionic/vue';
+import {IonContent, IonPage, onIonViewWillEnter, onIonViewWillLeave} from '@ionic/vue';
 import ProjectList from "@/components/ProjectList.vue";
+import {ref} from "vue";
+import {useProjects} from "@/composables/useProjects";
+
+const {loadProjects} = useProjects();
+
+let projectsLoaded = ref<boolean>(false);
+onIonViewWillEnter(async () => {
+  await loadProjects();
+  projectsLoaded.value = true;
+});
+onIonViewWillLeave(() => {
+  projectsLoaded.value = false;
+});
 </script>
